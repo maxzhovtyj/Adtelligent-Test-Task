5c83f296b084a8dcc0b06d41d55867d812dcec58
Zhovtaniuk Maxym <zhovtyjshady@gmail.com>
2023-03-08 11:37:13 +0200

    add: repo CreateSeller

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -1,9 +1,19 @@
 package repository
 
-import "database/sql"
+import (
+	"database/sql"
+	"github.com/maxzhovtyj/Adtelligent-Test-Task/internal/models"
+)
+
+const (
+	ordersTable    = "orders"
+	productsTable  = "products"
+	customersTable = "customers"
+	sellersTable   = "sellers"
+)
 
 type Users interface {
-	Create()
+	CreateSeller(seller models.Seller) error
 }
 
 type repository struct {
--- internal/repository/users_mysql.go
+++ internal/repository/users_mysql.go
@@ -1,6 +1,10 @@
 package repository
 
-import "database/sql"
+import (
+	"database/sql"
+	"fmt"
+	"github.com/maxzhovtyj/Adtelligent-Test-Task/internal/models"
+)
 
 type UsersRepo struct {
 	db *sql.DB
@@ -12,6 +16,13 @@ func NewUsersRepo(db *sql.DB) *UsersRepo {
 	}
 }
 
-func (r *UsersRepo) Create() {
+func (r *UsersRepo) CreateSeller(seller models.Seller) error {
+	queryInsertSeller := fmt.Sprintf("INSERT INTO %s (name, phone_number) VALUES (?, ?)", sellersTable)
 
+	row := r.db.QueryRow(queryInsertSeller, seller.Name, seller.PhoneNumber)
+	if row.Err() != nil {
+		return row.Err()
+	}
+
+	return nil
 }
