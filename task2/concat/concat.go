b75f41dbd0ad03a3352e687f9d600d13475b6f41
Zhovtaniuk Maxym <zhovtyjshady@gmail.com>
2023-03-09 22:30:57 +0200

    add: task 2 in readme

--- task2/concat/concat.go
+++ task2/concat/concat.go
@@ -14,11 +14,11 @@ func Basic(str []string) string {
 	return result
 }
 
-func WithJoin(sl []string) {
-	strings.Join(sl, "")
+func WithJoin(sl []string) string {
+	return strings.Join(sl, "")
 }
 
-func WithStringBuilder(sl []string, strLen int) {
+func WithStringBuilder(sl []string, strLen int) string {
 	var q strings.Builder
 
 	q.Grow(len(sl) * strLen)
@@ -26,4 +26,6 @@ func WithStringBuilder(sl []string, strLen int) {
 	for _, v := range sl {
 		q.WriteString(v)
 	}
+
+	return q.String()
 }
