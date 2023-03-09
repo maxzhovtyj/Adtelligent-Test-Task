8b6703f7d159c45e3c26a12e678b68db3fa4a63c
Zhovtaniuk Maxym <zhovtyjshady@gmail.com>
2023-03-09 22:08:15 +0200

    add: task 2

--- task2/concat/concat.go
+++ task2/concat/concat.go
@@ -1,7 +1,29 @@
 package concat
 
-import "strings"
+import (
+	"strings"
+)
 
-func Concat(sl []string) string {
-	return strings.Join(sl, "")
+func Basic(str []string) string {
+	result := ""
+
+	for _, v := range str {
+		result += v
+	}
+
+	return result
+}
+
+func WithJoin(sl []string) {
+	strings.Join(sl, "")
+}
+
+func WithStringBuilder(sl []string, strLen int) {
+	var q strings.Builder
+
+	q.Grow(len(sl) * strLen)
+
+	for _, v := range sl {
+		q.WriteString(v)
+	}
 }
--- task2/concat/concat__test.go
+++ task2/concat/concat__test.go
@@ -1,9 +1,46 @@
 package concat
 
-import "testing"
+import (
+	"testing"
+)
 
-func BenchmarkConcat(b *testing.B) {
-	for i := 0; i < b.N; i++ {
+var loremIpsum = `
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+Maecenas non odio eget quam gravida laoreet vitae id est. 
+Cras sit amet porta dui. Pellentesque at pulvinar ante. 
+Pellentesque leo dolor, tristique a diam vel, posuere rhoncus ex. 
+Mauris gravida, orci eu molestie pharetra, mi nibh bibendum arcu, in bibendum augue neque ac nulla. 
+Phasellus consectetur turpis et neque tincidunt molestie. 
+Vestibulum diam quam, sodales quis nulla eget, volutpat euismod mauris.
+`
 
+const SIZE = 10000
+
+var strSlice = make([]string, SIZE)
+
+func init() {
+	for i := 0; i < len(strSlice); i++ {
+		strSlice[i] = loremIpsum
 	}
+}
+
+func BenchmarkConcatenationOperator(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Basic(strSlice)
+	}
+	b.ReportAllocs()
+}
+
+func BenchmarkStringsJoin(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		WithJoin(strSlice)
+	}
+	b.ReportAllocs()
+}
+
+func BenchmarkStringBuilder(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		WithStringBuilder(strSlice, len(loremIpsum))
+	}
+	b.ReportAllocs()
 }
